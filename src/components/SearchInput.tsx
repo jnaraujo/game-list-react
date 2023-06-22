418a414f73d4637203635b453dbb78f19581abab
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-22 15:36:45 -0300

    ✨ feat: improved SearchInput

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,13 +1,18 @@
 import { Search } from "lucide-react"
 
-export default function SearchInput() {
+interface SearchInputProps {
+  onChange?: (value: string) => void
+}
+
+export default function SearchInput({ onChange }: SearchInputProps) {
   return (
-    <div className="flex h-full items-center justify-center gap-2 rounded-md bg-zinc-50 px-2">
+    <div className="flex h-8 w-full items-center justify-center gap-2 rounded-md bg-zinc-50 px-2">
       <Search size={24} className="text-zinc-400" />
       <input
         type="text"
+        onChange={(e) => onChange?.(e.target.value)}
         placeholder="Digite o nome do jogo"
-        className="placeholder:zinc-400 flex-1 bg-transparent text-zinc-600 focus:outline-none"
+        className="placeholder:zinc-400 w-full bg-transparent text-zinc-600 focus:outline-none"
       />
     </div>
   )
