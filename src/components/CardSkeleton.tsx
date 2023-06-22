dc2d989d685f52834b66284f64e097aa56e6712f
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-22 14:43:13 -0300

    ✨ feat: add CardSkeleton

---
+++ src/components/CardSkeleton.tsx
@@ -0,0 +1,16 @@
+export default function CardSkeleton() {
+  return (
+    <div className="relative flex animate-pulse overflow-hidden rounded-lg bg-white shadow-md md:flex-col">
+      <div className="h-full w-28 shrink-0 bg-zinc-300 md:h-56 md:w-full" />
+      <div className="flex flex-1 flex-col justify-between p-2">
+        <div>
+          <div className="h-3 w-16 bg-zinc-300" />
+          <div className="mt-1 h-6 w-44 bg-zinc-300" />
+          <div className="mt-1 h-16 bg-zinc-300" />
+        </div>
+
+        <div className="mt-2 h-8 rounded-lg bg-zinc-300" />
+      </div>
+    </div>
+  )
+}
