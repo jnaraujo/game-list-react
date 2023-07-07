796f49abb3f8cf408ed75134813a6510b3a8860a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-07-07 13:22:46 -0300

    ✨ feat: add virtulization to cards

--- src/components/Layouts/Cards.tsx
+++ src/components/Layouts/Cards.tsx
@@ -1,6 +1,8 @@
 import CardSkeleton from "../CardSkeleton"
 import type { ApiResponse } from "@/@types/api"
 import Card from "../Card"
+import { AutoSizer, List, WindowScroller } from "react-virtualized"
+import Breakpoints from "@/constants/breakpoints"
 
 interface Props {
   games: ApiResponse
@@ -19,32 +21,82 @@ export default function Cards({
   const shouldShowGameList = !isLoading && games.length > 0
 
   return (
-    <section className="container my-8 grid gap-12 md:grid-cols-3">
+    <section className="container my-8 flex">
       {isLoading ? (
-        <>
+        <div className="grid w-full gap-8 lg:grid-cols-3">
           <CardSkeleton />
           <CardSkeleton />
           <CardSkeleton />
-        </>
+        </div>
       ) : null}
 
-      {shouldShowGameList
-        ? games.map((game, index) => (
-            <Card
-              key={index}
-              id={game.id}
-              title={game.title}
-              description={game.short_description}
-              genre={game.genre}
-              image={game.thumbnail}
-              platform={game.platform}
-              releaseDate={new Date(game.release_date)}
-              url={game.freetogame_profile_url}
-              isLiked={likedGames?.[game.id] ?? false}
-              rating={ratedGames?.[game.id] ?? -1}
-            />
-          ))
-        : null}
+      {shouldShowGameList ? (
+        <WindowScroller>
+          {({ height, isScrolling, onChildScroll, scrollTop }) => (
+            <AutoSizer disableHeight>
+              {({ width }) => {
+                const PADDING = 32
+
+                const ITEM_HEIGHT =
+                  (width > Breakpoints.md ? 400 : 200) + PADDING
+                const ITEMS_COUNT = games.length
+
+                const itemsPerRow = width > Breakpoints.lg ? 3 : 1
+                const rowCount = Math.ceil(ITEMS_COUNT / itemsPerRow)
+
+                return (
+                  <List
+                    autoHeight
+                    isScrolling={isScrolling}
+                    onScroll={onChildScroll}
+                    scrollTop={scrollTop}
+                    width={width}
+                    height={height}
+                    rowCount={rowCount}
+                    rowHeight={ITEM_HEIGHT}
+                    offs
+                    rowRenderer={({ index, key, style }) => {
+                      const items = []
+
+                      for (
+                        let i = index * itemsPerRow;
+                        i < Math.min(itemsPerRow * (index + 1), ITEMS_COUNT);
+                        i++
+                      ) {
+                        items.push(games[i])
+                      }
+
+                      return (
+                        <div
+                          key={key}
+                          style={style}
+                          className="grid gap-8 lg:grid-cols-3"
+                        >
+                          {items.map((game) => (
+                            <Card
+                              key={game.id}
+                              id={game.id}
+                              title={game.title}
+                              description={game.short_description}
+                              image={game.thumbnail}
+                              platform={game.platform}
+                              genre={game.genre}
+                              releaseDate={new Date(game.release_date)}
+                              url={game.freetogame_profile_url}
+                              isLiked={likedGames?.[game.id] ?? false}
+                              rating={ratedGames?.[game.id] ?? -1}
+                            />
+                          ))}
+                        </div>
+                      )
+                    }}
+                  />
+                )
+              }}
+            </AutoSizer>
+          )}
+        </WindowScroller>
+      ) : null}
 
       {shouldShowGameNotFound ? (
         <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-lg text-zinc-800">
---
+++ src/constants/breakpoints.ts
@@ -0,0 +1,8 @@
+enum Breakpoints {
+  sm = 640,
+  md = 768,
+  lg = 1024,
+  xl = 1280,
+}
+
+export default Breakpoints
