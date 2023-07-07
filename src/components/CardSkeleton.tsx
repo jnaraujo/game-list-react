5d14b329d2daca77467893aa3714bd70e6d68e76
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-07-07 13:27:21 -0300

    ✨ feat: removed Card width

--- src/components/CardSkeleton.tsx
+++ src/components/CardSkeleton.tsx
@@ -1,6 +1,6 @@
 export default function CardSkeleton() {
   return (
-    <div className="relative flex h-[200px] w-full animate-pulse overflow-hidden rounded-lg bg-white shadow-md md:h-[400px] md:w-[350px] md:flex-col">
+    <div className="relative flex h-[200px] w-full animate-pulse overflow-hidden rounded-lg bg-white shadow-md md:h-[400px] md:flex-col">
       <div className="h-full w-28 shrink-0 bg-zinc-300 md:h-56 md:w-full" />
       <div className="flex flex-1 flex-col justify-between p-2">
         <div>
