d597395009c6c16a64f348dc498ac29ac50c4aef
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-07-10 12:13:02 -0300

    ✨ feat: improved Navbar

--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -8,12 +8,17 @@ export default function Navbar() {
   const { user } = useAuthContext()
 
   return (
-    <nav className="flex h-10 flex-wrap items-center justify-end bg-violet-900 px-4">
+    <nav className="flex h-10 flex-wrap items-center justify-between bg-violet-900 px-4">
+      <div>
+        <Link href="/" className="text-zinc-50 hover:text-zinc-200">
+          Game Search App
+        </Link>
+      </div>
       <div>
         {user ? (
           <div className="flex items-center gap-6">
             <DropdownMenu>
-              <span className="cursor-pointer text-zinc-200">
+              <span className="block max-w-[100px] cursor-pointer truncate text-zinc-200 md:max-w-fit">
                 {user.displayName || user.email}
               </span>
             </DropdownMenu>
