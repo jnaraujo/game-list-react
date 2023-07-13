62c20be38c62b1a6ed907519baa4590716ff7309
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-07-13 18:42:55 -0300

    ✨ feat: improved Error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image"
+import Button from "./Button"
 
 interface Props {
   message: string
 }
 export default function Error({ message }: Props) {
+  function handleReload() {
+    if (typeof window === "undefined") return
+    window.location.reload()
+  }
   return (
-    <div className="flex w-72 flex-col items-center justify-center gap-2 text-center">
+    <div className="m-4 flex w-72 flex-col items-center justify-center gap-2 text-center">
       <Image
         src="/cat.svg"
         alt="Gato comendo a página"
@@ -16,6 +21,9 @@ export default function Error({ message }: Props) {
         Ooops... Parece que o gato comeu a página!
       </h2>
       <p className="text-lg text-zinc-600">{message}</p>
+      <Button onClick={handleReload} className="px-4">
+        Recarregar a página
+      </Button>
     </div>
   )
 }
